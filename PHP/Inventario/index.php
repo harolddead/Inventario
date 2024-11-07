865fa8e91a22bc41ddf77a0613822f761089d0f8
harolddead <135444888+harolddead@users.noreply.github.com>
2024-11-07 16:35:22 -0500

    Add files via upload

---
+++ PHP/Inventario/index.php
@@ -0,0 +1,34 @@
+<?php require "./inc/session_start.php"; ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <?php include "./inc/head.php"; ?>
+</head>
+<body>
+    <?php
+            if(!isset($_GET['vista']) || $_GET['vista'] ==""  ){
+                $_GET['vista']="login";
+            }
+
+            if (is_file("./vistas/".$_GET['vista'].".php") && $_GET['vista']!="login" && $_GET['vista']!="404"){
+
+            include "./inc/navbar.php";
+
+            include "./vistas/".$_GET['vista'].".php";
+
+            include "./inc/script.php";
+
+        }else{
+            if($_GET['vista']=="login"){
+                include "./vistas/login.php";
+            }else{
+
+            }
+
+        }
+
+    ?>
+
+    <?php echo ' <p> </p>'; ?>
+</body>
+</html>
